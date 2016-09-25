ca5589252dc61ed0023df1b4ac1e5577d1a56c33
Alan Campbell <alancampbell4444@gmail.com>
2016-09-24 20:03:26 -0700

    s

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   after_initialize :downgrade
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def standard?
